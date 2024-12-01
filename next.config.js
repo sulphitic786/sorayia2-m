0391c562af8bcd1b1d30831de727e17001ed63ba
sorayia.com <161327848+ceosorayia@users.noreply.github.com>
2024-12-01 23:39:15 +0100

    sorayia.com

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
-const withVideos = require("next-videos");
-
-const nextConfig = withVideos({});
+const nextConfig = {
+  reactStrictMode: true, // Active le mode strict de React
+  eslint: {
+    ignoreDuringBuilds: true, // Ignore les erreurs ESLint lors du build
+  },
+  images: {
+    domains: ['example.com'], // Configuration pour les images externes
+  },
+};
 
 module.exports = nextConfig;
